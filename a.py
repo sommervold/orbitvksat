4f600d2e4ede1b2e9c70fd6a2315b1a16346ae96
Nicolai Sommervold <nicolai@sommervold.net>
2024-04-30 00:19:55 +0200

    stats

--- a.py
+++ a.py
@@ -79,13 +79,69 @@ for i in range(week_num + 1):
             ksat.append(member)
 
 
-longest = sorted(ksat + orbit, key=lambda x: x["distance"], reverse=True)[:10]
-highest = sorted(ksat + orbit, key=lambda x: x["elev_gain"], reverse=True)[:10]
 ksat_length = sum(x["distance"] for x in ksat)
 ksat_height = sum(x["elev_gain"] for x in ksat)
 
 orbit_length = sum(x["distance"] for x in orbit)
 orbit_height = sum(x["elev_gain"] for x in orbit)
+longest = sorted(ksat + orbit, key=lambda x: x["distance"], reverse=True)
+highest = sorted(ksat + orbit, key=lambda x: x["elev_gain"], reverse=True)
+
+i = 0
+for athlete in longest:
+    i += 1
+    athlete["length_rank"] = i
+
+i = 0
+for athlete in highest:
+    i += 1
+    athlete["height_rank"] = i
+
+
+with open("data/history.json", "r") as f:
+    history = json.load(f)
+
+for athlete in ksat + orbit:
+    id = str(athlete["athlete_id"])
+    
+    if id not in history["athletes"]:
+        history["athletes"][id] = {
+            "firstname": athlete["athlete_firstname"], 
+            "lastname": athlete["athlete_lastname"],
+            "picture": athlete["athlete_picture_url"],
+            "history": []
+        }
+    
+    athlete2 = history["athletes"][id]
+    athlete2["history"].append({
+        "time": time.isoformat(),
+        "distance": athlete["distance"],
+        "height": athlete["elev_gain"],
+        "moving_time": athlete["moving_time"],
+        "num_activities": athlete["num_activities"],
+        "velocity": athlete["velocity"],
+        "height_rank": athlete["height_rank"],
+        "length_rank": athlete["length_rank"],
+    })
+
+history["orbit"].append({
+    "time": time.isoformat(),
+    "distance": orbit_length,
+    "height": orbit_height,
+    "height_rank": 1 if ksat_height >= orbit_height else 2,
+    "length_rank": 1 if ksat_length >= orbit_length else 2,
+})
+
+history["ksat"].append({
+    "time": time.isoformat(),
+    "distance": ksat_length,
+    "height": ksat_height,
+    "height_rank": 1 if orbit_height >= ksat_height else 2,
+    "length_rank": 1 if orbit_length >= ksat_length else 2,
+})
+
+with open("data/history.json", "w") as f:
+    json.dump(history, f, indent=2)
 
 with open("data/latest.json", "w") as f:
     json.dump(
@@ -94,8 +150,8 @@ with open("data/latest.json", "w") as f:
             "ksat_height": ksat_height,
             "orbit_length": orbit_length,
             "ksat_length": ksat_length,
-            "longest": longest,
-            "highest": highest,
+            "longest": longest[:10],
+            "highest": highest[:10],
             "total_length": orbit_length + ksat_length,
             "total_height": orbit_height + ksat_height,
         },
